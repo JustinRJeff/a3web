146fbc39ef9af854b285cc031eefe0b541756f82
JustinRJeff <114168858+JustinRJeff@users.noreply.github.com>
2022-12-08 21:56:50 -0500

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -8,17 +8,7 @@
 function changeTxt() {
   document.getElementById("javstxt").innerHTML = "Just kidding";
 }
-// Change color functions
-// Changes color of text upon event 
-function changeClr(x) {
-    x.style-color; 'yellow'
-}
-function normClr(x) {
-  x.style-color; 'white'
-}
-function normClr() {
-  style-color; 'white'
-}
+
 // Change text size function
 function bigTxt() {
   document.getElementById('big').style.fontSize='100px';
@@ -26,30 +16,23 @@ function bigTxt() {
 // Alert functions
 // Causes alert upon event
 function alrt() {
-  alert("JavaScript alert")
+  alert("JavaScript alert");
 }
 function inpt() {
   alert("Invalid address");
 }
-// Key value function
-// Change text to key value
-function catyrs(event) {
-  document.getElementById("cat").innerHTML = event.keyCode;
-}
+
 
 
 
 // By Josh
 // Alert function for Geovisualization Page
 function meow() {
-  alert("Have you seen my baseball?")
+  alert("Have you seen my baseball?");
 }
 
 // Alert function for Spatial Analysis Page
 function meow() {
-  alert("Have you seen my Dad?")
+  alert("Have you seen my Dad?");
 }
-
-
-
 
